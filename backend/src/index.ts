6e6aae5bb9e87d297265cfe1fadea1ec6a291a4c
kaluva navya sri <tharunkumarkaluva@gmail.com>
2026-05-23 17:02:58 +0530

    fix api routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const PORT = Number(process.env.PORT) || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    origin:"*",
     credentials: true,
   })
 );
